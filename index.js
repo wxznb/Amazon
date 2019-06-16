891049e173a69b856ca2e1a0eaa1aa5161f6dddc
sarusama <ld839183343@163.com>
2019-06-16 23:44:41 +0800

    [update] amazonArea

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const app = require("./routes/index.js");
+const express = require("express");
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
 
 const server = app.listen(3000, function () {
     let host = server.address().address;
--- routes/selling.js
+++ routes/selling.js
@@ -2,11 +2,29 @@ const express = require("express");
 const router = express.Router();
 const Request = require("request");
 
+const getDomainUrl = "https://d2h8zr0m6mus4x.cloudfront.net/primesignup/package.json";
+const amazonAreaLetters = {
+    "us": "美国站",
+    "uk": "英国站",
+    "ca": "加拿大站",
+    "de": "德国站",
+    "es": "西班牙站",
+    "fr": "法国站",
+    "it": "意大利站",
+    "jp": "日本站",
+    "in": "印度站",
+    "cn": "中国站",
+    "sg": "新加坡站",
+    "mx": "墨西哥站",
+    "ae": "澳洲站"
+};
+
 router.post("*", function (request, responce, next) {
     responce.header("Access-Control-Allow-Origin", "*");
 
     next();
 });
+
 router.get("*", function (request, responce, next) {
     responce.header("Access-Control-Allow-Origin", "*");
 
@@ -15,7 +33,6 @@ router.get("*", function (request, responce, next) {
 
 router.get("/", function (request, responce, next) {
     let domain = new Object();
-    const getDomainUrl = "https://d2h8zr0m6mus4x.cloudfront.net/primesignup/package.json";
 
     Request({
         url: getDomainUrl,
@@ -25,6 +42,7 @@ router.get("/", function (request, responce, next) {
              requestResponce.statusCode === 200 ) {
             data = JSON.parse(data);
             domain = Object.assign(domain, data["domain"]);
+
             let sellingUrl = "/gp/offer-listing/";
             let asinCode = "B07H97YKQS";
 
@@ -43,6 +61,33 @@ router.get("/", function (request, responce, next) {
                 } else {
                     responce.send(error);
                 }
+            });
+        }
+    });
+});
+
+router.get("/amazonArea", function (request, responce, next) {
+    Request({
+        url: getDomainUrl,
+        method: "GET"
+    }, function (error, requestResponce, data) {
+        if ( !error &&
+             requestResponce.statusCode === 200 ) {
+            data = JSON.parse(data);
+            let area = new Array();
+            
+            Object.keys(data["domain"]).forEach(function (key) {
+                area.push(amazonAreaLetters[key]);
+            });
+
+            responce.send({
+                code: 0,
+                data: area
+            });
+        } else {
+            responce.send({
+                code: 1001,
+                message: error
             });
         }
     });
